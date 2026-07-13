d146b20453fcb42ce68080f4c95b3e42c02adde2
yamari0005 <yamari0005@gmail.com>
2026-07-13 18:25:27 +0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 // @ts-ignore
-import pinoHttp from 'pino-http';
+const pinoHttp = require('pino-http');
 import router from "./routes";
 import { logger } from "./lib/logger";
 
